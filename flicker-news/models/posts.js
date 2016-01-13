e5c7b80183b7f161cf1fac3342fd76ab7024b3b9
Andriy <andkulak@gmail.com>
2016-01-12 23:46:10 -0500

    Added posts and comments routes

--- flicker-news/models/posts.js
+++ flicker-news/models/posts.js
@@ -10,4 +10,9 @@ var postSchema = new mongoose.Schema({
 	comments: [{type: mongoose.Schema.Types.ObjectId, ref: 'Comment'}]
 });
 
+postSchema.methods.upvote = function(cb) {
+	this.upvotes += 1;
+	this.save(cb);
+};
+
 mongoose.model('Post', postSchema);
--- flicker-news/routes/index.js
+++ flicker-news/routes/index.js
@@ -1,9 +1,105 @@
+/**
+ * All of our Routes are defined here
+ */
+
 var express = require('express');
 var router = express.Router();
+var mongoose = require('mongoose');
+var Post = mongoose.model('Post');
+var Comment = mongoose.model('Comment');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+});
+
+
+// GET Route for all posts
+router.get('/posts', function(req, res, next){
+  Post.find(function(err, posts){
+    if(err) { return next(err); }
+
+    res.json(posts);
+  });
+});
+
+// POST Route for a new post
+router.post('/posts/', function(req, res, next){
+  var post = new Post(req.body);
+
+  post.save(function(err,post){
+    if(err) {return next(err);}
+  });
+});
+
+
+
+// Whenever posts are detected within parameters, they will be loaded from db
+router.param('posts', function(req,res,next,id) {
+  var query = Post.findById(id);
+
+  query.exec(function(err, post){
+    if (err) { return next(err); }
+    if (!post) { return next(new Error("can't find post")); }
+  });
+
+});
+
+// Whenever comments are detected within parameters, they will be loaded from db
+router.param('comment', function(req,res,next,id) {
+  var query = Comment.findById(id);
+
+  query.exec(function(err, post){
+    if (err) { return next(err); }
+    if (!post) { return next(new Error("can't find comment")); }
+  })
+
+});
+
+// GET Route gets specific post information by id
+router.get('/posts/:post', function(req, res) {
+
+  //loads all comments for specific post
+  req.posts.populate('comments', function(err,post){
+    res.json(post);
+  });
+
+  res.json(req.post);
+});
+
+//PUT Request to update upvote for post. Remember it is at 0 as a default unless otherwise adjusted
+router.put('/posts/:post/upvote', function(req,res,next){
+  req.post.upvote(function(err,post){
+    if(err) { return next(err); }
+
+    res.json(post);
+  })
+});
+
+//PUT Request to update upvote for comment. Remember it is at 0 as a default unless otherwise adjusted
+router.put('/posts/:post/comments/:comment/upvote', function(req,res,next){
+  req.comment.upvote(function(err,comment){
+    if(err) { return next(err); }
+
+    res.json(comment);
+  })
+});
+
+//PUT Request to update upvote. Remember it is at 0 as a default unless otherwise adjusted
+router.put('/posts/:post/comments', function(req,res,next){
+  var comment = new Comment(req.body);
+  comment.post = req.post;
+
+  comment.save(function(err,comment){
+    if(err) { return next(err); }
+
+    req.post.comments.push(comment);
+    req.post.save(function(err, post){
+      if(err) { return next(err); }
+
+      res.json(comment);
+    })
+  });
 });
 
 module.exports = router;

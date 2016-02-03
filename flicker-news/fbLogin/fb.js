c2613816df320393fbc21afdc4d281a2b15e2984
Andriy <andkulak@gmail.com>
2016-02-02 21:55:31 -0500

    deleted excess files and cleaned up code

--- flicker-news/fbLogin/fb.js
+++
@@ -1,6 +0,0 @@
-// facebook app settings - fb.js
-module.exports = {
-	'appID' : '185245021524028',
-	'appSecret' : 'c453a2cfd024157eb1a9a569cf946a37',
-	'callbackUrl' : 'http://localhost:3000/login/facebook/callback'
-};
